ef66f9069148be4f4f97db7bb92bdfabe33cc1ab
Yodhaa <saurabhsingh4589@gmail.com>
2020-10-18 13:32:48 +0530

    CI Pipeline Added

--- app/src/main/java/com/saurabh/mytodolistappservice/fragments/list/ListFragment.kt
+++ app/src/main/java/com/saurabh/mytodolistappservice/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.saurabh.mytodolistappservice.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.saurabh.mytodolistappservice.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -27,7 +25,15 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set Menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
+        //super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
--- app/src/main/java/com/saurabh/mytodolistappservice/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/saurabh/mytodolistappservice/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.saurabh.mytodolistappservice.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.saurabh.mytodolistappservice.R
 
 
@@ -15,8 +13,17 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Set Menu Option
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu,menu)
+        //super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
