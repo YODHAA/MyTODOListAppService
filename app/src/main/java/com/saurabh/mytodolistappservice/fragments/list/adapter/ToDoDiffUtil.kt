36bb546db4dc14664d004f1e27b4dd7d857e031f
Yodhaa <saurabhsingh4589@gmail.com>
2020-10-24 17:59:59 +0530

    Recycler DiffUtil class Functionality Implmented

--- app/src/main/java/com/saurabh/mytodolistappservice/fragments/list/adapter/ToDoDiffUtil.kt
+++ app/src/main/java/com/saurabh/mytodolistappservice/fragments/list/adapter/ToDoDiffUtil.kt
@@ -1,6 +1,28 @@
 package com.saurabh.mytodolistappservice.fragments.list.adapter
 
-class ToDoDiffUtil  {
+import androidx.recyclerview.widget.DiffUtil
+import com.saurabh.mytodolistappservice.data.models.ToDoData
+
+class ToDoDiffUtil( private val oldList : List<ToDoData> , private  val newList: List<ToDoData>) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+       return newList.size
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return  oldList[oldItemPosition].id == newList[newItemPosition].id && oldList[oldItemPosition].title == newList[newItemPosition].title
+                && oldList[oldItemPosition].description == newList[newItemPosition].description &&
+                oldList[oldItemPosition].priority == newList[newItemPosition].priority
+
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
 
 
 }
